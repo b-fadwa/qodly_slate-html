bc9866e653c3d77fddd5d2cb5d8c129175c19f14
Fadwa BAYED <fadwa.bayed@4d.com>
2026-06-15 11:53:32 +0100

    add i18n support for infos bubbles

--- src/components/TextEditor/UI/Toolbar.tsx
+++ src/components/TextEditor/UI/Toolbar.tsx
@@ -23,32 +23,32 @@ interface ToolbarProps {
 const Toolbar: FC<ToolbarProps> = ({ readonly }) => {
   return (
     <div id="toolbar" className="flex w-full flex-wrap gap-2 border-b p-2">
-      <ToolbarInfoPopover label="Bold">
+      <ToolbarInfoPopover label="textEditor_bold">
         <MarkButton icon={MdOutlineFormatBold} format="bold" readonly={readonly} />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Italic">
+      <ToolbarInfoPopover label="textEditor_italic">
         <MarkButton icon={MdOutlineFormatItalic} format="italic" readonly={readonly} />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Underline">
+      <ToolbarInfoPopover label="textEditor_underline">
         <MarkButton icon={MdOutlineFormatUnderlined} format="underline" readonly={readonly} />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Strikethrough">
+      <ToolbarInfoPopover label="textEditor_strikethrough">
         <MarkButton icon={MdOutlineStrikethroughS} format="strikethrough" readonly={readonly} />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Font size (px)">
+      <ToolbarInfoPopover label="textEditor_fontSize">
         <FontSizeButton icon={MdOutlineFormatSize} readonly={readonly} />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Text color">
+      <ToolbarInfoPopover label="textEditor_textColor">
         <ColorPickerButton icon={MdOutlineFormatColorText} readonly={readonly} format="color" />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Background color">
+      <ToolbarInfoPopover label="textEditor_backgroundColor">
         <ColorPickerButton
           icon={MdOutlineFormatColorFill}
           readonly={readonly}
           format="backgroundColor"
         />
       </ToolbarInfoPopover>
-      <ToolbarInfoPopover label="Clear formatting">
+      <ToolbarInfoPopover label="textEditor_clearFormatting">
         <ClearButton icon={MdOutlineFormatClear} readonly={readonly} />
       </ToolbarInfoPopover>
     </div>
